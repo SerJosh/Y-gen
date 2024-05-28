75cdab97e195387f08f3d5ec2e8db697d2dc242b
SerJosh <joshronan25@gmail.com>
2024-05-28 11:39:50 +0000

    Add secret key

--- yugen/settings.py
+++ yugen/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pm^d-ixi-(c@6e&8ysy-2cuw6^)0*j1h$sn!k3d1bom&m=nwch'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
