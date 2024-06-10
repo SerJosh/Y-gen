8b4bb15384ef9e69a62614a8c13467a46c43226b
SerJosh <joshronan25@gmail.com>
2024-06-10 22:24:49 +0000

    Add checkout views and templates

--- yugen/settings.py
+++ yugen/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'home',
     'pictures',
     'bag',
-    'checkout'
+    'checkout',
+    'crispy_forms',
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -52,6 +54,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yugen.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -64,8 +70,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
