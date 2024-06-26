b35c8587f5dda6c27b62aa57eb355a787448eca4
Joshua Ronan <joshronan25@gmail.com>
2024-06-26 18:33:29 +0000

    Add update model, views and urls

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Update
+from . import models
 
 # Register your models here.
+class UpdateAdmin(admin.ModelAdmin):
+    update_display = ('heading_update')
+
+admin.site.register(Update, UpdateAdmin)
--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Update
 
 # Create your views here.
 def index(request):
     """ A view to return the index page """
+    update = Update.objects.all()
+
+    context = {
+        'update': update,
+    }
     
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
+
+
